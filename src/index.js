179c496ead198bac94963b07ff28bfc88a6a140d
Boyd <boydgraves1@gmail.com>
2020-10-19 12:40:31 -0500

    Added components directory and index.js, added import statements.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { React } from "react";
+import { ReactDOM } from "react-dom";
+import { App } from "./components/apps.jsx";
+
+ReactDOM.render(<App text="hi" />, document.getElementById('root'));
